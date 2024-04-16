ab7ee763a6842988103e398ce4ec4a9b105a1aaa
hchang604 <henry.chang@alianza.com>
2024-04-15 21:26:06 -0700

    add navigation styling

--- App.tsx
+++ App.tsx
@@ -13,8 +13,23 @@ export default function App() {
     <>
       <StatusBar style="dark" />
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: '#351401',
+            },
+            headerTintColor: 'white',
+            contentStyle: {
+              backgroundColor: '#3f2f25',
+            },
+          }}>
+          <Stack.Screen
+            name="MealsCategories"
+            options={{
+              title: 'All Categories',
+            }}
+            component={CategoriesScreen}
+          />
           <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
         </Stack.Navigator>
       </NavigationContainer>
