93f67b61363581f78b56e2c6826f891f81d4f1ed
hchang604 <henry.chang@alianza.com>
2024-04-17 20:59:56 -0700

    implement meals overview nav header

--- screens/MealsOverviewScreen.tsx
+++ screens/MealsOverviewScreen.tsx
@@ -1,25 +1,41 @@
-import React from 'react';
-import {MEALS} from '../data/dummy-data';
+import React, {useLayoutEffect} from 'react';
+import {CATEGORIES, MEALS} from '../data/dummy-data';
 import {FlatList, ListRenderItemInfo, StyleSheet, View} from 'react-native';
-import {RouteProp, useRoute} from '@react-navigation/native';
+import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
 import {RootStackParamList} from './CategoriesScreen';
 import MealItem from '../components/MealItem';
 import Meal from '../models/meal';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 
 type MealsOverviewProps = RouteProp<RootStackParamList, 'MealsOverview'>;
 
 function MealsOverviewScreen() {
   const route = useRoute<MealsOverviewProps>();
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+
   const categoryId = route.params.categoryId;
 
   const displayedMeals = MEALS.filter((meal) =>
     meal.categoryIds.find((id) => id === categoryId),
   );
 
-  console.log(displayedMeals);
   function renderMealItem(item: ListRenderItemInfo<Meal>) {
     return <MealItem itemData={item} />;
   }
+
+  /* useLayoutEffect makes transition of setting navigation title look nicer */
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === categoryId,
+    )?.title;
+
+    if (categoryTitle) {
+      navigation.setOptions({
+        title: categoryTitle,
+      });
+    }
+  }, [categoryId, navigation]);
 
   return (
     <View style={styles.container}>
