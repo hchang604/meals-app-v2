7eab69e3177be70f75c06de4ab771969ce714bdd
hchang604 <henry.chang@alianza.com>
2024-04-29 22:51:12 -0700

    implement draaer nav

--- App.tsx
+++ App.tsx
@@ -2,12 +2,39 @@ import React from 'react';
 import {StatusBar} from 'expo-status-bar';
 import {StyleSheet} from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
+import FavouritesScreen from './screens/FavouritesScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
 import {NavigationContainer} from '@react-navigation/native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#351401',
+        },
+        headerTintColor: 'white',
+        sceneContainerStyle: {
+          backgroundColor: '#3f2f25',
+        },
+      }}>
+      <Drawer.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{
+          title: 'All Categories',
+        }}
+      />
+      <Drawer.Screen name="Favourites" component={FavouritesScreen} />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -25,14 +52,20 @@ export default function App() {
             },
           }}>
           <Stack.Screen
-            name="MealsCategories"
+            name="Drawer"
             options={{
-              title: 'All Categories',
+              headerShown: false,
             }}
-            component={CategoriesScreen}
+            component={DrawerNavigator}
           />
           <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
-          <Stack.Screen name="MealDetail" component={MealDetailScreen} />
+          <Stack.Screen
+            name="MealDetail"
+            component={MealDetailScreen}
+            options={{
+              title: 'Meal Info',
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
