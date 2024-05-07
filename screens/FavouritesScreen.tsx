e50806a9e030a2845a0bd7d16f5ffac19cdf23f6
hchang604 <henry.chang@alianza.com>
2024-05-06 22:24:17 -0700

    implement favourites screen

--- screens/FavouritesScreen.tsx
+++ screens/FavouritesScreen.tsx
@@ -1,8 +1,37 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useContext} from 'react';
+import MealsList from '../components/MealList/MealsList';
+import {FavoritesContext} from '../store/context/favorites-context';
+import {MEALS} from '../data/dummy-data';
+import {StyleSheet, Text, View} from 'react-native';
 
 function FavouritesScreen() {
-  return <Text>Favourites Screen </Text>;
+  const favoriteMealsCtx = useContext(FavoritesContext);
+  const favoriteMeals = MEALS.filter((meal) =>
+    favoriteMealsCtx.ids.includes(meal.id),
+  );
+
+  if (favoriteMeals.length === 0) {
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>You Have No Favorite Meals yet</Text>
+      </View>
+    );
+  }
+
+  return <MealsList items={favoriteMeals} />;
 }
 
 export default FavouritesScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
